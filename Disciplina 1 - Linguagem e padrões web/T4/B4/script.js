2717127cf34b61284a2b6319c4224b7a5a701c6e
MichaelMagalhaes <60831491+MichaelMagalhaes@users.noreply.github.com>
2024-03-22 21:06:08 -0300

    Add files via upload

---
+++ Disciplina 1 - Linguagem e padrões web/T4/B4/script.js
@@ -0,0 +1,11 @@
+document.querySelector('#h1element').innerHTML = "Texto H1";
+document.querySelector('.h2element').innerHTML = "Texto H2";
+document.querySelector('h3').innerHTML = "Texto H3";
+
+function alerta (){
+    alert("Alerta");
+}
+
+function consolepessoal(){
+    console.log('Alerta console');
+}
